039ac223dcca353f176ca853f56f987028ec910c
Dinesh Ban <eternalsharinganeye@gmail.com>
2022-07-03 20:43:10 +0545

    navbar complete

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Article = () => {
+  return (
+    <div><h1>article</h1></div>
+  )
+}
+
+export default Article
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Brand = () => {
+  return (
+    <div><h1>Brand</h1></div>
+  )
+}
+
+export default Brand
