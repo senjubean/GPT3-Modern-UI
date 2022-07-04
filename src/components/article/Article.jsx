75b9bf168ad16ffe53e89beaad987dd356cba98b
Dinesh Ban <eternalsharinganeye@gmail.com>
2022-07-04 19:50:23 +0545

    add blog and footer section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
-const Article = () => {
+import './article.css'
+const Article = ({imgUrl, date, title}) => {
   return (
-    <div><h1>article</h1></div>
+    <div className="gpt3__blog-container_article">
+      <div className='gpt3__blog-container_article-image'>
+        <img src={imgUrl} alt="" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article
+        </p>
+      </div>
+
+    </div>
   )
 }
 
