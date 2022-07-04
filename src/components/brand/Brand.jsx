646d3d290afc9fb9e8692257086db05071c0bd1a
Dinesh Ban <eternalsharinganeye@gmail.com>
2022-07-04 16:26:58 +0545

    add brandm, features,what is GPT and future

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { google, slack, shopify, atlassian, dropbox } from './import'
+import './brand.css'
 
 const Brand = () => {
   return (
-    <div><h1>Brand</h1></div>
-  )
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+    )
 }
 
 export default Brand
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import logo from '../../assets/logo.svg';
 const Menu = () => (
     <>
         <p><a href="#home">Home</a></p>
-        <p><a href="#wgpt3">What is GPT?</a></p>
+        <p><a href="#whatgpt3">What is GPT?</a></p>
         <p><a href="#possibility">Open AI</a></p>
         <p><a href="#features">Case Studies</a></p>
         <p><a href="#blog">Library</a></p>
