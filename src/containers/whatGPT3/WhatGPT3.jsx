3cba18e9ab5084a4606e2519d73e76e9028ca7e3
Dinesh Ban <eternalsharinganeye@gmail.com>
2022-07-04 17:01:59 +0545

    add cta section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -4,7 +4,7 @@ import Feature from '../../components/feature/Feature'
 import './whatGPT3.css'
 const WhatGPT3 = () => {
   return (
-    <div className="gpt3__whatgpt3 section_margin" id="whatgpt3">
+    <div className="gpt3__whatgpt3 section__padding" id="whatgpt3">
       <div className="gpt3__whatgpt3-features">
         <Feature title="What is GPT-3?" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
       </div>
@@ -17,7 +17,6 @@ const WhatGPT3 = () => {
       </div>
       <div className="gpt3__whatgpt3-container">
         <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "/>
-        
         <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
         <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"/>
   
